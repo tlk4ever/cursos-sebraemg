2785db35facd1e23d035d68d4fcd09f3a948c8d2
Thiago Brito <tbffreitas@gmail.com>
2017-08-19 00:21:22 -0300

    migrations

---
+++ backend/database/migrations/2017_08_19_020319_create_address_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('street');
+            $table->integer('number');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
---
+++ backend/database/migrations/2017_08_19_030743_create_class_theme_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassThemeTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('class-theme', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('category_id')->unsigned();;
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->decimal('price', 10, 2);
+            $table->datetime('start');
+            $table->datetime('finish');
+            $table->integer('consultant_id')->unsigned();;
+            $table->foreign('consultant_id')->references('id')->on('consultant');
+            $table->integer('address_id')->unsigned();;
+            $table->foreign('address_id')->references('id')->on('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('class-theme');
+    }
+
+}
